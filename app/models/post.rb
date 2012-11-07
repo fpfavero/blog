1ca226cf270f9322c8365dc9c82a6ecc6e7940b2
Felipe Pereira Fávero <felipe.713@gmail.com>
2012-11-07 02:28:44 -0200

    Just testing changes of syntax

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   attr_accessible :content, :name, :title
 
   validates :name, :title, presence:true
-  validates :title, :length => { minimum:5 }
+  validates :title, length: { minimum:6 }
 
 end
