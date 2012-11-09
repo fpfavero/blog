691812e636da97c779ae6a720af2c57f9ca542e2
Felipe Pereira Fávero <felipe.713@gmail.com>
2012-11-09 00:34:37 -0200

    Incluindo os comentarios, nao finalizado

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :name, :title
 
+  has_many :comments
+
   validates :name, :title, presence:true
   validates :title, length: { minimum:6 }
 
